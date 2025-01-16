9a3837f23a8d29e104f6e22b5e00813933a0a221
Aranil <52273848+Aranil@users.noreply.github.com>
2025-01-16 14:36:57 +0100

    Remove .idea folder and unnecessary files

--- build/lib/dbflow/src/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This is a core module to allow database functionality
-
-- create SQLite DB if not exist
-- connect and maintain connection
-- create Tables (tables should be defined manually in db_structure.py script)
-- ...
-
-"""
