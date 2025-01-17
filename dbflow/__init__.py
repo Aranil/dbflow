2ce725d2ebd28702cf1f1f41f2fc05b384456318
Aranil <arslanova@uni-jena.de>
2025-01-17 13:42:31 +0100

    added description of teh purpose of db_structure.py file

---
+++ dbflow/__init__.py
@@ -0,0 +1,4 @@
+"""
+The db_structure.py file can be customized to define table schemas. Additionally, custom scripts can be added here to
+extend or tailor the application for specific use cases.
+"""
